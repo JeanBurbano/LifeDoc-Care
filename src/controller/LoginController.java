efa71df99d2a540b0bf1f60f608ca529353a8a7b
JeanBurbano <jeancarlosburbanopantoja5678@gmail.com>
2026-06-25 07:30:27 -0500

    logica de login ya casi termina

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -16,31 +16,33 @@ public class LoginController implements ActionListener, MetodosPublicos {
         this.lg.bRegistar.addActionListener(this);
         this.lg.bIngresar.addActionListener(this);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.lg.bIngresar) {
-            final String user = this.lg.getId();
-            final String contrasena = this.lg.getPassword();
-
-            if (!(user.isEmpty() && user.isEmpty())) {
-                boolean verificadoru = false;
-                if (user.matches("\\d{10}")) {
-                    verificadoru = true;
-                } else if (user.length() > 10) {
-                    JOptionPane.showMessageDialog(lg, "El campo id no cumple con longitud de 10");
-                } else {
-                    JOptionPane.showMessageDialog(lg, "El campo id tiene caracteres no validos");
-                }
-
-                if (contrasena.matches("[a-zA-Z0-9/$#?!%]{8,10}") && verificadoru) {
+            String user = this.lg.getId();
+            String contrasena = this.lg.getPassword();
+            if (!user.isEmpty() && !contrasena.isEmpty()) {
+                if (user.matches("\\d{10}") && contrasena.matches("(?=.{8,10}$)[a-zA-Z0-9]+([/$#?!%][a-zA-Z0-9]*)*")) {
                     JOptionPane.showMessageDialog(lg, "iniciaste sesion");
-                } else if (contrasena.contains(" ") || Pattern.compile("[^a-zA-Z0-9/$#?!%]").matcher(contrasena).find()) {
-                    JOptionPane.showMessageDialog(lg, "La contrasena tiene caracteres no validos");
-                } else if (contrasena.length() < 8 || contrasena.length() > 10) {
-                    JOptionPane.showMessageDialog(lg, "La contrasena debe ser como minimo 8 caracteres y maximo 10");
+                } else {
+                    if (Pattern.compile("[^0-9]").matcher(user).find()) {
+                        JOptionPane.showMessageDialog(lg, "El campo id contiene caracteres no validos");
+                    }
+                    if (user.length() < 10 || user.length() > 10) {
+                        JOptionPane.showMessageDialog(lg, "El campo id no cumple con longitud de 10");
+                    }
+                    if (Pattern.compile("[^a-zA-Z0-9/$#?!%]").matcher(contrasena).find()) {
+                        JOptionPane.showMessageDialog(lg, "La contrasena tiene caracteres no validos");
+                    } 
+                    if (Pattern.compile("[/$#?!%]{2}").matcher(contrasena).find()) {
+                        JOptionPane.showMessageDialog(lg, "La contrasena No se permiten simbolos consecutivos");
+                    }
+                    if (contrasena.length() < 8 || contrasena.length() > 10) {
+                        JOptionPane.showMessageDialog(lg, "La contrasena debe ser como minimo 8 caracteres y maximo 10");
+                    }
                 }
-            } else if (user.isEmpty() && user.isEmpty()) {
+            } else if (user.isEmpty() && contrasena.isEmpty()) {
                 JOptionPane.showMessageDialog(lg, "Los dos campos son obligatorios");
             } else if (user.isEmpty()) {
                 JOptionPane.showMessageDialog(lg, "El Campo Id es obligatorio");
