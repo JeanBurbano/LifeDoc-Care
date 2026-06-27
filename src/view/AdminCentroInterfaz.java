645caecd5f107528cb83af1c2203d40609e3ab51
LuannyCossio <alejandracossiolv@gmail.com>
2026-06-26 20:17:10 -0500

    Se creo la interfaz AdminCentroInterfaz junto a los botones del personal del centro, el inventario de medicamentos y el registrar personal

---
+++ src/view/AdminCentroInterfaz.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author lunaa
+ */
+public class AdminCentroInterfaz extends PacienteInterfaz{
+    public JButton btnPersonalCentro, btnInventarioMedicamentos, btnregistrarUsuario;
+    
+    public AdminCentroInterfaz(String nombrePersona, String nombreInterfaz, String url){
+        super(nombrePersona, nombreInterfaz,url);
+        this.btnPersonalCentro = new JButton("👥 Personal del Centro");
+        this.btnregistrarUsuario = new JButton("Registrar Personal");
+        this.btnInventarioMedicamentos = new JButton("💊 Inventario de Medicamentos");
+        super.agregarBotonCuerpo1(btnPersonalCentro);
+        super.agregarBotonCuerpo1(btnregistrarUsuario);
+        super.agregarBotonCuerpo1(btnInventarioMedicamentos);
+        
+    }
+    
+}
