b82a5449f88b92b59d3aaf69e82df5bc66ac6d1f
LuannyCossio <alejandracossiolv@gmail.com>
2026-06-26 21:58:58 -0500

    Se creo la interfaz de administrador del centro junto a los botones con los dos apartados de personal del centro y inventario de medicaentos. Tambien se puso el metodo para que cuando se ingrese a un apartado, el boton de ese apartado quede inhabilitado

--- src/view/AdminCentroInterfaz.java
+++ src/view/AdminCentroInterfaz.java
@@ -11,18 +11,33 @@ import javax.swing.JPanel;
  *
  * @author lunaa
  */
-public class AdminCentroInterfaz extends PacienteInterfaz{
+public class AdminCentroInterfaz extends PacienteInterfaz {
+
     public JButton btnPersonalCentro, btnInventarioMedicamentos, btnregistrarUsuario;
-    
-    public AdminCentroInterfaz(String nombrePersona, String nombreInterfaz, String url){
-        super(nombrePersona, nombreInterfaz,url);
+
+    public AdminCentroInterfaz(String nombrePersona, String nombreInterfaz, String url) {
+        super(nombrePersona, nombreInterfaz, url);
         this.btnPersonalCentro = new JButton("👥 Personal del Centro");
         this.btnregistrarUsuario = new JButton("Registrar Personal");
         this.btnInventarioMedicamentos = new JButton("💊 Inventario de Medicamentos");
         super.agregarBotonCuerpo1(btnPersonalCentro);
         super.agregarBotonCuerpo1(btnregistrarUsuario);
         super.agregarBotonCuerpo1(btnInventarioMedicamentos);
+
+    }
+
+    @Override
+    protected void habilitarBotonesMenu(JButton botonActivo) {
+        if (botonActivo != btnPersonalCentro && !btnPersonalCentro.isEnabled()) {
+            this.btnPersonalCentro.setEnabled(true);
+        }
+        if (botonActivo != btnInventarioMedicamentos && !btnInventarioMedicamentos.isEnabled()) {
+            this.btnInventarioMedicamentos.setEnabled(true);
+        }
+    }
+
+    public void mostrarVistaPersonalCentro() {
         
     }
-    
+
 }
