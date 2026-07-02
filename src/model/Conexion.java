c390332c1195872a4c8f8741a581c53eb0e68f9e
JeanBurbano <jeancarlosburbanopantoja5678@gmail.com>
2026-07-01 23:51:07 -0500

    clase conexion

--- src/model/Conexion.java
+++ src/model/Conexion.java
@@ -7,14 +7,14 @@ import javax.swing.JOptionPane;
 public class Conexion {
 
     Connection conexion;
-    String url, user, password;
-    
-    public Connection getConection(){
+
+    public Connection getConection() {
         try {
-            Class.forName("");
-            conexion = DriverManager.getConnection(url, user, password);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost/bd_lifedoccare", "root", "");
+            JOptionPane.showMessageDialog(null, "Conectado");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,e.toString() ,"Base de datos apagada"+e.getMessage(),JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, e.toString(), "Base de datos apagada" + e.getMessage(), JOptionPane.ERROR_MESSAGE);
         }
         return conexion;
     }
