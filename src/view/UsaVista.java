573ae0727f4fe18bc74b5fab9dc9d4b53d8990f3
JeanBurbano <jeancarlosburbanopantoja5678@gmail.com>
2026-07-21 04:43:36 +0200

    se creo crear usuarios del centro interfaz

--- src/view/UsaVista.java
+++ src/view/UsaVista.java
@@ -1,8 +1,14 @@
 package view;
 
+import static java.awt.Frame.MAXIMIZED_BOTH;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
 public class UsaVista {
 
     public static void main(String[] args) {
-        
+        RegistoPersonalInterfaz p = new RegistoPersonalInterfaz("dsadasdas");
+         p.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        p.setExtendedState(MAXIMIZED_BOTH);
+        p.setVisible(true);
     }
 }
